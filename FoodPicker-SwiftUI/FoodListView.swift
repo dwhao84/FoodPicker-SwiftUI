157f4f91ca97be9ea3a9831138e1d54bec68fb27
Dawei Hao <dwsamurai84@gmail.com>
2025-07-25 07:52:18 +0800

    加入Geometry Reader的內容，完成課程課程15 - ChaoCode。

--- FoodPicker-SwiftUI/FoodListView.swift
+++ FoodPicker-SwiftUI/FoodListView.swift
@@ -8,11 +8,161 @@
 import SwiftUI
 
 struct FoodListView: View {
+    @Environment(\.editMode) var editMode
+    @Environment(\.dynamicTypeSize) var textSize
+    
+    @State private var food = Food.examples
+    @State private var selectedFood = Set<Food.ID>()
+    
+    @State private var shouldShowSheet = false
+    @State private var foodDetailHeight: CGFloat = FoodDetailSheetHeightKey.defaultValue
+    
+    var isEditing: Bool {
+        editMode?.wrappedValue == .active
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            titleBar
+            
+            List($food, editActions: .all, selection: $selectedFood) { $food in
+                HStack {
+                    Text(food.name).padding(.vertical, 10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            if isEditing { return }
+                            shouldShowSheet = true
+                        }
+                    
+                    if isEditing {
+                        Image(systemName: "pencil")
+                            .font(.title2.bold())
+                            .foregroundColor(.cyan)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .padding(.horizontal)
+            
+        }.background(.groupBG)
+            .safeAreaInset(edge: .bottom, content: buildFloatButton)
+            .sheet(isPresented: $shouldShowSheet) {
+                let food = food.first!
+                let shouldVStack = textSize.isAccessibilitySize || food.image.count > 1
+
+                AnyLayout.useVStack(if: shouldVStack, spacing: 30) {
+                    Text(food.image)
+                        .font(.system(size: 100))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                    
+                    Grid(horizontalSpacing: 12, verticalSpacing: 12) {
+                        buildNutritionView(title: "熱量", value: food.$carb)
+                        buildNutritionView(title: "蛋白質", value: food.$protein)
+                        buildNutritionView(title: "脂肪", value: food.$fat)
+                        buildNutritionView(title: "碳水", value: food.$carb)
+                    }
+                }
+                .padding()
+                .padding(.vertical)
+                .overlay {
+                    GeometryReader { proxy in
+                        Color.clear.preference(key: FoodDetailSheetHeightKey.self, value: proxy.size.height)
+                        
+                    }
+                }
+                .onPreferenceChange(FoodDetailSheetHeightKey.self) {
+                    foodDetailHeight = $0
+                }
+                .presentationDetents([.height(foodDetailHeight)])
+            }
+    }
+}
+
+private extension FoodListView {
+    struct FoodDetailSheetHeightKey: PreferenceKey {
+        static var defaultValue: CGFloat = 0
+        
+        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+            value = nextValue()
+        }
+    }
+}
+
+private extension FoodListView {
+    var titleBar: some View {
+        HStack {
+            Label("食物清單", systemImage: "fork.knife")
+                .font(.title.bold())
+                .foregroundColor(.cyan)
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            EditButton()
+                .buttonStyle(.bordered)
+                .foregroundColor(.cyan)
+                .environment(\.locale, Locale(identifier: "zh_TW"))
+        } .padding()
+    }
+    
+    var addButton: some View {
+        Button {} label: {
+            Image(systemName: "plus.circle.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.white, .cyan)
+                .padding()
+        }
+    }
+    
+    var removeButton: some View {
+        Button {
+            withAnimation {
+                food = food.filter { !selectedFood.contains($0.id)}
+            }
+        } label: {
+            Text("刪除全部")
+                .font(.title2.bold())
+                .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .mainButtonStyle(shape: .roundedRectangle(radius: 8))
+        .foregroundStyle(.groupBG)
+        .padding(.horizontal, 50)
+    }
+    
+    func buildFloatButton() -> some View {
+        ZStack {
+            removeButton
+                .transition(.move(edge:.leading))
+                .opacity(isEditing ? 1 : 0)
+                .id(isEditing)
+            
+            HStack {
+                Spacer()
+                addButton
+                    .scaleEffect(isEditing ? 0.0001 : 1)
+                    .opacity(isEditing ? 0 : 1)
+                    .animation(.easeInOut, value: isEditing)
+                    .id(isEditing)
+            }
+        }
+    }
+    
+    func buildNutritionView(title: String, value: String) -> some View {
+        GridRow {
+            Text(title).gridCellAnchor(.leading)
+            Text(value).gridCellAnchor(.trailing)
+        }
     }
 }
 
 #Preview {
     FoodListView()
 }
+
+//// 用這段來測試 針對不同狀態 來設計畫面
+//struct FoodListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FoodListView().environment(\.editMode, .constant(.active))
+//    }
+//}
